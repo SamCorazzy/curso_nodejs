ce390847584864057b307312d00df33b9ec1804c
Samuel Perez Reyes <corazzy@hotmail.com>
2021-11-01 19:58:40 -0600

    Se agregó express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola Samuel! >.<");
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req,res) { res.send('Hola Samuel!!! >.<'); });
+
+app.listen(3000);
+//localhost:3000
